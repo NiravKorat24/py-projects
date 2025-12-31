1715d9362409b45834d7cd9380f00155513461c2
NiravKorat24 <koratnirav07@gmail.com>
2025-12-31 22:17:13 +0530

    Add files via upload

---
+++ adventure.py
@@ -0,0 +1,46 @@
+name = input("Type your name: ")
+print("Welcome", name, "to this adventure!")
+
+answer = input(
+    "You are on a dirt road. It has come to an end and you can go left or right.\n"
+    "Which way would you like to go? (left/right) "
+).lower()
+
+if answer == "left":
+    answer = input(
+        "You come to a river. You can walk around it or swim across.\n"
+        "Type 'walk' to walk around or 'swim' to swim across: "
+    ).lower()
+
+    if answer == "swim":
+        print("You swam across and were eaten by an alligator.")
+    elif answer == "walk":
+        print("You walked for many miles, ran out of water, and lost the game.")
+    else:
+        print("Not a valid option. You lose.")
+
+elif answer == "right":
+    answer = input(
+        "You come to a bridge. It looks wobbly.\n"
+        "Do you want to cross it or head back? (cross/back) "
+    ).lower()
+
+    if answer == "back":
+        print("You go back and lose.")
+    elif answer == "cross":
+        answer = input(
+            "You cross the bridge and meet a stranger.\n"
+            "Do you talk to them? (yes/no) "
+        ).lower()
+
+        if answer == "yes":
+            print("You talk to the stranger and they give you gold. YOU WIN!")
+        elif answer == "no":
+            print("You ignore the stranger. They are offended and you lose.")
+        else:
+            print("Not a valid option. You lose.")
+    else:
+        print("Not a valid option. You lose.")
+
+else:
+    print("Not a valid option. You lose.")
